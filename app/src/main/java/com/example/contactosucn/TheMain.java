3d314afa9c9b2c5e3b74d4ba4a7a2b5cc209fb77
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2021-12-02 02:31:57 -0300

    fix(Scrapper): create main method to use the scrapper

--- app/src/main/java/com/example/contactosucn/TheMain.java
+++ app/src/main/java/com/example/contactosucn/TheMain.java
@@ -1,7 +1,6 @@
 package com.example.contactosucn;
 
 
-
 import com.example.contactosucn.model.Contact;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -9,28 +8,33 @@ import com.google.gson.reflect.TypeToken;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-public class Scrapper {
 
-  private final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+public final class TheMain {
+
+  private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
 
-  public ArrayList<Contact> getContactsFromUrl() throws IOException {
+  public static void main(String[] args) throws IOException {
+
 
     ArrayList<Contact> list;
-
+    String filename="contacts.json";
+    Path pathToFile = Paths.get(filename);
     // create a reader
-    Reader reader = Files.newBufferedReader(Paths.get("contacts.json"));
-
+    Reader reader = Files.newBufferedReader(pathToFile.toAbsolutePath());
     list = GSON.fromJson(reader, new TypeToken<ArrayList<Contact>>() {
     }.getType());
 
-    int start = list.get(list.size() - 1).getId();
+    int start = list.get(list.size() - 1).getId() + 1;
     int end = 30000;
 
     for (int id = start; id < end; id++) {
@@ -41,6 +45,8 @@ public class Scrapper {
           .get();
 
       if (doc.getElementById("lblNombre").text().length() < 1) {
+
+        System.out.println("id: "+ id);
         continue;
       }
 
@@ -59,19 +65,20 @@ public class Scrapper {
       String phone = doc.getElementById("lblTelefono").text();
       String office = doc.getElementById("lblOficina").text();
       String address = doc.getElementById("lblDireccion").text();
-
+      System.out.println(name + "added.");
+      list.add(
+          new Contact(id, name, charge, unit, email, phone, office, address));
       // Save by 15.
       if (list.size() % 15 == 0) {
-
-        GSON.toJson(list, new FileWriter(String.valueOf(Paths.get("contacts.json"))));
-
-        list.add(
-            new Contact(id, name, charge, unit, email, phone, office, address));
-
+        System.out.println("saved 15 contacts.");
+        Writer writer = new FileWriter("contacts.json");
+        GSON.toJson(list, writer);
+        // close writer
+        writer.close();
       }
+
     }
 
-    return list;
-
   }
+
 }
